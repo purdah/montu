305aaf7a98ac5216a5f92b4456ae68f481851591
millsmi <mike@themills.id.au>
2024-10-25 16:24:32 +1000

    Please refer to README.changelog.md

---
+++ README.changelog.md
@@ -0,0 +1,7 @@
+Task 1: Get the code to comile/run tests
+
+Notes: The initial code was failing to run tests as there was missing code in the index.ts file that 
+copied over values from the value returned from the getPlaceAutocomplete
+
+The tests now run, but that does not mean they are working ;-)
+
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ export async function getAutoCompleteDetails(address: any): Promise<any> {
     const apiKey = process.env.TOMTOM_API_KEY;
     // get autocomplete results
     const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY, address).then(async (autocompleteResults) => {
-        const res = []
-        return res
+        const mappedResults = autocompleteResults.map((result: any) => {
+            return {
+                placeId: result.placeId,
+                streetNumber: '',
+                countryCode: '',
+                country: '',
+                freeformAddress: '',
+                municipality: '',
+            };
+        });
+        return mappedResults;
     })
     // loop over and get details and map results
     return res
